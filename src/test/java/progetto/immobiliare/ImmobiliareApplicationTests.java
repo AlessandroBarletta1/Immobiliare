68d45ea85faf42c0df3384d545c158d569a54bf9
Alessandro Barletta <alessandrobarletta@MacBook-Pro-di-Alessandro-2.local>
2023-03-06 17:29:52 +0100

    Immobiliare1

---
+++ src/test/java/progetto/immobiliare/ImmobiliareApplicationTests.java
@@ -0,0 +1,13 @@
+package progetto.immobiliare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmobiliareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
